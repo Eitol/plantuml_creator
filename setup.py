b7d04aa16458e72fa164cf547884e397658e0435
Hector L Oliveros L <ext_hloliveros@falabella.cl>
2020-01-16 11:41:31 -0300

    fix setup

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires=[
         "attrs==19.3.0",
         "importlib-metadata==1.3.0",
-        "more-itertools==8.0.2"
+        "more-itertools==8.0.2",
         "packaging==20.0",
         "pluggy==0.13.1",
         "py==1.8.1",
