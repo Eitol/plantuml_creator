4135136a2a35f4801380f75f998881184ac2580a
Hector L Oliveros L <ext_hloliveros@falabella.cl>
2020-01-19 14:11:16 -0300

    add class to deflate and compress

--- plantuml_creator/__init__.py
+++ plantuml_creator/__init__.py
@@ -1,4 +1,4 @@
 """
  Version of the package
 """
-__version__ = "1.0.5"
+__version__ = "1.0.6"
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="plantuml_creator",
-    version="1.0.5",
+    version="1.0.7",
     description="Library to generate plantuml code from python objects",
     long_description=README,
     long_description_content_type="text/markdown",
